5ea8335c7a47cc622e1ff5bd41275af0be552b0c
VirtualPi <75913378+VirtualPi@users.noreply.github.com>
2020-12-14 17:18:02 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+// https://www.jenniferbland.com/saving-data-to-mongodb-database-from-node-js-application-tutorial/
+
+const express = require('express')
+const app =express()
+const mongoose =require ('mongoose')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const Article = require('./models/article')
+const bodyParser = require('body-parser');
+
+
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser : true, useUnifiedTopology: true, useCreateIndex: true}, )
+mongoose.Promise = global.Promise;
+
+var nameSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+app.post("/addaccount", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+      .then(item => {
+        res.send("item saved to database");
+      })
+      .catch(err => {
+        res.status(400).send("unable to save to database");
+    });
+});
+
+var User = mongoose.model("User", nameSchema)
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res)=>{
+    const articles = await Article.find().sort({ 
+        createdAt: 'desc' })
+        res.render('articles/index', {articles: articles})
+})
+app.use('/articles', articleRouter)
+
+app.listen(3000)
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
